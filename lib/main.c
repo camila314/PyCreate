fe1bacc2b316b29b53e4e14728c38ed04228edc1
camila314 <47485054+camila314@users.noreply.github.com>
2024-05-03 13:16:40 -0500

    maybe works on windows now???

--- lib/main.c
+++ lib/main.c
@@ -1,3 +1,4 @@
+#if __APPLE__
 #include <CoreFoundation/CoreFoundation.h>
 
 static char response[1024];
@@ -22,3 +23,34 @@ char* sendMessage(char const* text) {
 
 	return response;
 }
+#else
+#include <Windows.h>
+
+char* sendMessage(char const* text) {
+	// Send a message to the named pipe "GeodeIPCPipe"
+
+	DWORD dw;
+	std::string response;
+
+	HANDLE hPipe = CreateFile(TEXT("\\\\.\\pipe\\GeodeIPCPipe"), 
+	                   GENERIC_READ | GENERIC_WRITE, 
+	                   0,
+	                   NULL,
+	                   OPEN_EXISTING,
+	                   0,
+	                   NULL);
+
+	if (hPipe != INVALID_HANDLE_VALUE) {
+	    WriteFile(hPipe,
+	              text,
+	              strlen(text) + 1,
+	              &dw,
+	              NULL);
+	    ReadFile(hPipe, response, 1024, &dw, NULL);
+
+	    CloseHandle(hPipe);
+	}
+
+	return response;
+}
+#endif
